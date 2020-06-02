75768d732e5dda3a96d65e2cd98a24b86533dfe2
Lewis Kabui <lewisemm@users.noreply.github.com>
2020-06-02 13:02:04 +0300

    Convert <Books> to a class component
    - Convert to a class component so that the state of 'shelf' can
    be stored within this components. This will help us keep track of
    any book's current shelf.

--- src/Book.js
+++ src/Book.js
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import * as BooksAPI from './BooksAPI'
 
-function Book(props) {
-  const imageLinks = props.book.imageLinks
-  const thumbnailURL = imageLinks && imageLinks.thumbnail ? imageLinks.thumbnail : ''
-  const authors = props.book.authors ? props.book.authors : []
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnailURL})` }}></div>
-        <div className="book-shelf-changer">
-          <select value={ props.book.shelf } onChange={(e) => props.onCategoryChange(e, props.book)}>
-            <option value="move" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+  class Book extends Component {
+    static propTypes = {
+      book: PropTypes.object.isRequired,
+      onCategoryChange: PropTypes.func.isRequired
+    }
+
+    state = {
+      shelf: 'none'
+    }
+
+    componentDidMount = () => {
+      BooksAPI.get(this.props.book.id)
+        .then(book => {
+          this.setState({
+            shelf: book.shelf
+          })
+        })
+    }
+
+    handleShelfChange = (event) => {
+      // updates the book object's shelf property
+      this.props.onCategoryChange(event, this.props.book)
+      // updates the shelf state property for this component
+      this.setState({
+        shelf: event.target.value
+      })
+    }
+
+    render = () => {
+      const imageLinks = this.props.book.imageLinks
+      const thumbnailURL = imageLinks && imageLinks.thumbnail ? imageLinks.thumbnail : ''
+      const authors = this.props.book.authors ? this.props.book.authors : []
+      return (
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${thumbnailURL})` }}></div>
+            <div className="book-shelf-changer">
+              <select value={ this.state.shelf } onChange={this.handleShelfChange}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{ this.props.book.title }</div>
+          { authors.map((author, index) => {
+            return <div key={index} className="book-authors">{ author }</div>
+          })}
         </div>
-      </div>
-      <div className="book-title">{ props.book.title }</div>
-      { authors.map((author, index) => {
-        return <div key={index} className="book-authors">{ author }</div>
-      })}
-      
-    </div>
-  )
-}
-
-Book.propTypes = {
-  book: PropTypes.object.isRequired,
-  onCategoryChange: PropTypes.func.isRequired
+      )
+    }
 }
 
 export default Book;
